af8ab0e92f9f86069483c708b1c3c484b4b0d531
Yuri <yuri.manahan@gmail.com>
2017-02-16 16:39:31 -0500

    Completed stretch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+Knowledge Check:
+1) Get
+2) 115ms
+3) 200 ok
+4) regular
--- ajax.js
+++ ajax.js
@@ -1,5 +1,81 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+  $('#one').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      // data: ,
+      dataType: 'text',
+  }).done(function(responseData){
+    console.log('Success!');
+  });
+});
+
+$('#two').on('click', function(){
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/pong',
+    method: 'GET',
+    // data: ,
+    dataType: 'text',
+  }).done(function(responseData){
+    console.log(responseData);
+    var reply = document.createElement('p');
+    reply.append(responseData);
+    $('#step3456').append(reply);
+  }).fail(function(jqXHR, textStatus, errorThrown){
+    console.log(jqXHR);
+    // console.log(textStatus);
+    // console.log(errorThrown);
+    var fail = document.createElement('p');
+    fail.append(jqXHR.responseText);
+    $('#step3456').append(fail);
+  })
+  // .always(function(){
+  //   console.log("Request completed my friend!");
+  // });
+  });
+
+  $('#three').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      // data: ,
+      dataType: 'text',
+    }).done(function(responseData){
+      console.log(responseData);
+      var num = document.createElement('p');
+      num.append(responseData);
+      $('#step7').append(num);
+    });
+  });
+
+  $('#four').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data:{timezone: $('#time').val() } ,
+      dataType: 'text',
+    }).done(function(responseData){
+      console.log(responseData);
+      var num = document.createElement('p');
+      num.append(responseData);
+      $('#step8').append(num);
+    });
+  });
+
+  
+  $('#five').on('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/a_car',
+      method: 'GET',
+      dataType: 'html',
+    }).done(function(responseData){
+      console.log(responseData);
+      var list = document.createElement('ul');
+      list.setAttribute('id', 'car')
+      list.append(responseData);
+      $('#step9').append(list);
+    });
+  });
 
 });
